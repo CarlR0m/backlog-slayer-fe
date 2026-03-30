f8f8ccbade2f40ba3d22987e79cf7584ff2ef0f0
CarlR0m <carlromprogram@gmail.com>
2026-03-30 20:37:48 +0200

    reestructuración y avance en login/registro-wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //Rutas a las que usuarios no logueados o registrados tienen acceso
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  // ...
+
+  // Rutas a las que usuarios logueados tienen acceso
+
+  //Ruta comodin en caso de que no se encuentre ninguna ruta
+  {
+    path: '**',
+    redirectTo: 'auth',
+  }
+];
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  company: {
+    name: 'Backlog Slayer',
+    logo: '',
+  },
+  api: {
+    url: 'http://localhost:8000/api',
+  }
+};
