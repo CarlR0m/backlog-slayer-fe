1edb41b4131f74fa0b3faa2a5148ee10190afe69
CarlR0m <carlromprogram@gmail.com>
2026-04-13 18:04:35 +0200

    AME-888

--- src/app/back-log-slayer/pages/steam-callback/steam-callback.ts
+++ src/app/back-log-slayer/pages/steam-callback/steam-callback.ts
@@ -23,8 +23,7 @@ export class SteamCallBack implements OnInit {
     if (steamStatus === 'success') {
       this.executeSync();
     } else {
-      this.errorMessage.set(reason || 'La autenticación falló.');
-      this.status.set('error');
+      this.handleReason(reason!);
     }
   }
   private executeSync() {
@@ -34,11 +33,30 @@ export class SteamCallBack implements OnInit {
         this.status.set('success');
       },
       error: (err) => {
-        this.errorMessage.set(err.error?.message || 'Error al sincronizar.');
-        console.error('Error de sincronización con Steam:', err);
+        this.errorMessage.set(err.error?.message || 'Error en la sincronización con Steam.');
         this.status.set('error');
       }
     });
+  }
+  handleReason(reason: string){
+      switch (reason) {
+        case 'already_linked':
+          this.errorMessage.set('La cuenta de Steam ya está vinculada a otra cuenta de Backlog Slayer.');
+          break;
+        case 'invalid_signature':
+          this.errorMessage.set('La solicitud no es válida.');
+          break;
+        case 'access_denied':
+          this.errorMessage.set('No se ha concedido acceso a Steam.');
+          break;
+        case 'server_error':
+          this.errorMessage.set('Error del servidor.');
+          break;
+        default:
+          this.errorMessage.set('La autenticación falló.');
+          break;
+      }
+      this.status.set('error');
   }
   goToLibrary() {
     this.router.navigateByUrl('/back-log-slayer/games-library');
